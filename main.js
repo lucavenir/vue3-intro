10549ed45007a3529cd36a1a8d717fd001d5c15b
venir.dev <luca@venir.dev>
2022-08-02 14:57:43 +0200

    One-way data binding for images

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      product: 'Socks',
-      description: "Dr. Seuss' obsession."
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg'
+        }
     }
-  }
-});
+})

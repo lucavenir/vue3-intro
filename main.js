c396c69d2fc29659efc55a42594721ed3c4c4fd9
venir.dev <luca@venir.dev>
2022-08-02 15:32:59 +0200

    Disabling a button

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ const app = Vue.createApp({
             product: 'Socks',
             brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
-            inStock: true,
+            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+            ],
         }
     },
     methods: {

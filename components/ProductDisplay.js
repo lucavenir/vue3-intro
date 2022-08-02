b6ffc1345e17035e4aaf83fb2eafcec0d3a35bef
venir.dev <luca@venir.dev>
2022-08-02 16:28:14 +0200

    Emitting events

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -5,36 +5,43 @@ app.component('product-display', {
       required: true
     }
   },
-  template: /*html*/
-    `<div class="product-display">
-      <div class="product-container">
-        <div class="product-image">
-          <img v-bind:src="image">
-        </div>
-        <div class="product-info">
-          <h1>{{ title }}</h1>
-
-          <p v-if="inStock">In Stock</p>
-          <p v-else>Out of Stock</p>
-
-          <p>Shipping: {{ shipping }}</p>
-
-          <ul>
-            <li v-for="detail in details">{{ detail }}</li>
-          </ul>
-
-          <div 
-            v-for="(variant, index) in variants" 
-            :key="variant.id" 
-            @mouseover="updateVariant(index)" 
-            class="color-circle" 
-            :style="{ backgroundColor: variant.color }">
-          </div>
-          
-          <button class="button" :class="{ disabledButton: !inStock }" :disabled="!inStock" v-on:click="addToCart">Add to Cart</button>
-        </div>
+  template: 
+  /*html*/
+  `<div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img v-bind:src="image">
       </div>
-    </div>`,
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+
+        <p>Shipping: {{ shipping }}</p>
+
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+
+        <div 
+          v-for="(variant, index) in variants" 
+          :key="variant.id" 
+          @mouseover="updateVariant(index)" 
+          class="color-circle" 
+          :style="{ backgroundColor: variant.color }">
+        </div>
+        
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          v-on:click="addToCart">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  </div>`,
   data() {
     return {
         product: 'Socks',
@@ -48,26 +55,28 @@ app.component('product-display', {
     }
   },
   methods: {
-    addToCart() {
-        this.cart += 1
-    },
-    updateVariant(index) {
-        this.selectedVariant = index
-    }
+      addToCart() {
+          this.$emit('add-to-cart')
+      },
+      updateVariant(index) {
+          this.selectedVariant = index
+      }
   },
   computed: {
-    title() {
-        return this.brand + ' ' + this.product
-    },
-    image() {
-        return this.variants[this.selectedVariant].image
-    },
-    inStock() {
-        return this.variants[this.selectedVariant].quantity
-    },
-    shipping() {
-      if (this.premium) return 'Free';
-      else return '€ 2.99';
-    }
+      title() {
+          return this.brand + ' ' + this.product
+      },
+      image() {
+          return this.variants[this.selectedVariant].image
+      },
+      inStock() {
+          return this.variants[this.selectedVariant].quantity
+      },
+      shipping() {
+        if (this.premium) {
+          return 'Free'
+        }
+        return 2.99
+      }
   }
-});
+})

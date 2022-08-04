3f1778cb5625c495ea20c6c561f85e99d00a0bce
venir.dev <luca@venir.dev>
2022-08-04 12:25:17 +0200

    Implemented a ""snackbar"" at the top

--- vue3-router/src/views/event/Register.vue
+++ vue3-router/src/views/event/Register.vue
@@ -6,9 +6,13 @@
 <script>
 export default {
   props: ['event'],
+  inject: ['GStore'],
   methods: {
     register() {
       // call some api... etc.
+
+      this.GStore.flashMessage = `You've successfully registered for ${this.event.title}`
+      setTimeout(() => (this.GStore.flashMessage = ''), 3000)
 
       this.$router.push({ name: 'EventDetails' })
     }

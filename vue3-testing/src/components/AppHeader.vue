00a282315c4d96e18f89213768d6ec901f49c73a
venir.dev <luca@venir.dev>
2022-08-04 17:20:11 +0200

    initalized a Unit Test Project
    + written a first test

---
+++ vue3-testing/src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- Output -->
+    <button v-show="loggedIn">Logout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loggedIn: false, // Input
+    };
+  },
+};
+</script>

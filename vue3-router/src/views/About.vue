1ea8bde4d4a5369cfd780fdeec3d8e5aa986aec8
venir.dev <luca@venir.dev>
2022-08-04 12:00:24 +0200

    Some more experiments

--- vue3-router/src/views/About.vue
+++ vue3-router/src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <p>A site for events to better the world.</p>
+    <button @click="goBack">Bo gack</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    goBack() {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
--- vue3-router/src/views/event/Register.vue
+++ vue3-router/src/views/event/Register.vue
@@ -1,9 +1,17 @@
 <template>
   <p>Register to your event</p>
+  <button @click="register">Register now!</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register() {
+      // call some api... etc.
+
+      this.$router.push({ name: 'EventDetails' })
+    }
+  }
 }
 </script>

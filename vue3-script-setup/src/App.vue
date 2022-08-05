ff9f89443779e5b0788adb4f56e49a121cafe64f
venir.dev <luca@venir.dev>
2022-08-05 12:00:20 +0200

    Type-safe refs with script setup

--- vue3-script-setup/src/App.vue
+++ vue3-script-setup/src/App.vue
@@ -1,19 +1,34 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue';
+import { onMounted, reactive, ref } from "vue";
+import fetchCount from "./fetchCount";
+
+interface AppInfo {
+  name: string;
+  slogan: string;
+}
+
+const count = ref<number | null>(null);
+
+const appInfo: AppInfo = reactive({
+  name: 'My App',
+  slogan: 'This is awesome.'
+});
+
+onMounted(() => {
+  fetchCount((c) => {
+    count.value = c
+  })
+});
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>{{ appInfo.name }}</h1>
+    <h2>{{ appInfo.slogan}}</h2>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <p>{{ count }}</p>
 </template>
 
 <style scoped>
